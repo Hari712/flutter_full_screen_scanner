bc375597d039edd5f427963b7d9ce93d5742aac8
Harshit Darji <harshit@avdevs.com>
2026-06-23 18:44:59 +0530

    fix: resolve static analyzer warnings for path dependencies and unused imports

--- flutter_full_screen_scanner_platform_interface/lib/flutter_full_screen_scanner_platform_interface.dart
+++ flutter_full_screen_scanner_platform_interface/lib/flutter_full_screen_scanner_platform_interface.dart
@@ -1,4 +1,3 @@
-library flutter_full_screen_scanner_platform_interface;
 
 export 'src/flutter_full_screen_scanner_platform_interface.dart';
 export 'src/messages.g.dart';
--- flutter_full_screen_scanner_platform_interface/test/flutter_full_screen_scanner_platform_interface_test.dart
+++ flutter_full_screen_scanner_platform_interface/test/flutter_full_screen_scanner_platform_interface_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_full_screen_scanner_platform_interface/flutter_full_screen_scanner_platform_interface.dart';
-
 void main() {
   test('platform interface dummy test', () {
     expect(true, true);
